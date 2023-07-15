cb2e627c234f5e7517235421ded195642a5e8e77
Polyxen1a <maslowa@inbox.ru>
2023-07-16 00:11:21 +0300

    Initial commit

---
+++ src/main/java/pro/sky/hibernateworkingwithmultipletablesrepeat/HibernateWorkingWithMultipleTablesRepeatApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.hibernateworkingwithmultipletablesrepeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateWorkingWithMultipleTablesRepeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateWorkingWithMultipleTablesRepeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/hibernateworkingwithmultipletablesrepeat/HibernateWorkingWithMultipleTablesRepeatApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.hibernateworkingwithmultipletablesrepeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateWorkingWithMultipleTablesRepeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
